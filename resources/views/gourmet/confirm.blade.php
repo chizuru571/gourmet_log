4cbd2992d551fb67a93b40471883564b8f31a5c8
chizuru <rcbu7410@gmail.com>
2023-09-19 20:52:24 +0000

    fix/header

---
+++ resources/views/gourmet/confirm.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.gourmet')
+@section('title'){{ $gourmet->shop_name }}@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>{{ $gourmet->shop_name }}詳細</h2>
+                    <div class="form-group row">
+                        <label class="col-md-3">店名</label>
+                        <div class="col-md-9">
+                            <p class="shop_name mx-auto">{{ $gourmet->shop_name }}</p>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3">店名 フリガナ</label>
+                        <div class="col-md-9">
+                            <p class="name_katakana mx-auto">{{ $gourmet->name_katakana }}</p>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3">カテゴリー</label>
+                        <div class="col-md-9">
+                            <p class="category mx-auto">{{ $gourmet->category }}</p>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3">レビュー</label>
+                        <div class="col-md-9">
+                            <p class="review mx-auto">{{ $gourmet->review }}</p>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-9">料理写真</label>
+                        <div class="image col-md-3">
+                                @if ($gourmet->food_picture)
+                                    <img src="{{ secure_asset('storage/image/' . $gourmet->food_picture) }}">
+                                @endif
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3">Google Map URL</label>
+                        <div class="col-md-9">
+                            <p class="map_url mx-auto">{{ $gourmet->map_url }}</p>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3">電話番号</label>
+                        <div class="col-md-9">
+                            <p class="tel mx-auto">{{ $gourmet->tel }}</p>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3">コメント</label>
+                        <div class="col-md-9">
+                            <p class="comment mx-auto">{{ $gourmet->comment }}</p>
+                        </div>
+                    </div>
+                    @csrf
+                    <div class ="row">
+                    <div class="col-12 d-flex justify-content-center">
+                    <div class="row mt-3">
+                        <div class="col-md-5">
+                            <a href="{{ route('gourmet.index')}}">
+                            <input type="submit" class="btn btn-success" value="戻る">
+                            </a>
+                        </div>
+                        <div class="col-md-2">　</div>
+                        <div class="col-md-5">
+                            <input type="hidden" name="id" value="{{ $gourmet_form->id }}">
+                            @csrf
+                            <input type="submit" class="btn btn-primary" value="登録する">
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+            </div>
+        </div>
+    </div>
+@endsection
